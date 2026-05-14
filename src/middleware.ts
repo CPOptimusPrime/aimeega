6fde02276b68777d02fdbbd05b74167b7e85f92e
CPOptimusPrime <ai.optimusprime.bot@gmail.com>
2026-05-14 15:48:43 +0200

    Fix middleware redirect to login

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,6 +2,13 @@ import { authMiddleware } from "@clerk/nextjs"
 
 export default authMiddleware({
   publicRoutes: ["/", "/login", "/signup", "/api/webhooks(.*)"],
+  ignoredRoutes: ["/api/webhooks(.*)"],
+  afterAuth(auth, req) {
+    if (!auth.userId && !auth.isPublicRoute) {
+      const loginUrl = new URL('/login', req.url)
+      return Response.redirect(loginUrl)
+    }
+  }
 })
 
 export const config = {
